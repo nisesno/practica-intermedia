52f7c6fcb544c059ff588c8f2a43a3ac7beb5213
nisesno <nicosesmero11@gmail.com>
2025-11-14 18:52:17 +0100

    busqueda y resultados

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 
 function App() {
   const [busqueda, setBusqueda] = useState('')
+  const [series, setSeries] = useState([])
+
+  const buscarSeries = async () => {
+    const respuesta = await fetch(`https://api.tvmaze.com/search/shows?q=${busqueda}`)
+    const datos = await respuesta.json()
+      setSeries(datos)
+  }
 
   return (
     <div>
@@ -12,7 +19,15 @@ function App() {
         value={busqueda}
         onChange={(e) => setBusqueda(e.target.value)}
       />
-      <button>Buscar</button>
+      <button onClick={buscarSeries}>Buscar</button>
+
+      <h2>Resultados</h2>
+      {series.map((item) => (
+        <div key={item.show.id}>
+          <img src={item.show.image ? item.show.image.medium : 'https://via.placeholder.com/210x295'} />
+          <h3>{item.show.name}</h3>
+        </div>
+      ))}
     </div>
   )
 }
