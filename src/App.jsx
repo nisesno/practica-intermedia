03eb31633bae989bd69e55a1098703eecd2ffcf8
nisesno <nicosesmero11@gmail.com>
2025-11-15 16:35:28 +0100

    favoritos

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import { useState } from 'react'
 function App() {
   const [busqueda, setBusqueda] = useState('')
   const [series, setSeries] = useState([])
+  const [favoritos, setFavoritos] = useState([])
 
   const buscarSeries = async () => {
     const respuesta = await fetch(`https://api.tvmaze.com/search/shows?q=${busqueda}`)
     const datos = await respuesta.json()
       setSeries(datos)
+  }
+
+  const agregarFavorito = (serie) => {
+  setFavoritos([...favoritos, serie])
+  }
+
+  const quitarFavorito = (id) => {
+    const nuevos = favoritos.filter(fav => fav.show.id != id)
+    setFavoritos(nuevos)
+  }
+
+  const esFavorito = (id) => {
+    return favoritos.some(fav => fav.show.id == id)
   }
 
   return (
@@ -26,6 +40,18 @@ function App() {
         <div key={item.show.id}>
           <img src={item.show.image ? item.show.image.medium : 'https://via.placeholder.com/210x295'} />
           <h3>{item.show.name}</h3>
+          <button onClick={() => esFavorito(item.show.id) ? quitarFavorito(item.show.id) : agregarFavorito(item)}>
+            {esFavorito(item.show.id) ? '❤️' : '🤍'}
+          </button>
+        </div>
+      ))}
+
+      <h2>Mis Favoritos</h2>
+      {favoritos.map((item) => (
+        <div key={item.show.id}>
+          <img src={item.show.image ? item.show.image.medium : 'https://via.placeholder.com/210x295'} />
+          <h3>{item.show.name}</h3>
+          <button onClick={() => quitarFavorito(item.show.id)}>❤️</button>
         </div>
       ))}
     </div>
